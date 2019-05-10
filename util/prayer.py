aa09700455d2bf2707fdd92e91dddbfd2e921f51
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-05-10 10:50:10 -0700

    fix indentation

--- util/prayer.py
+++ util/prayer.py
@@ -5,16 +5,15 @@ import logger
 
 log = logger.get_logger(__name__)
 
+
 def play(name=None):
     if not name:
         name = 'azan.mp3'
-	path = os.path.dirname(os.path.abspath(__file__))     
+    path = os.path.dirname(os.path.abspath(__file__))
     file_path = '{}/../assets/{}'.format(path, name)
     log.info('Calling Azan now')
     log.info(file_path)
     os.system('cvlc --play-and-exit {}'.format(file_path))
-
-
 
 
 def format_azan_time(azan_time):
