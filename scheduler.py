a07174777299e85459386c769832a78d6c73cb40
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-05-10 12:06:51 -0700

    Use pygame to play sound

--- scheduler.py
+++ scheduler.py
@@ -10,43 +10,37 @@ log = logger.get_logger(__name__)
 
 AZAN_ENUM = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
 
+
 def main():
-	scheduler = sched.scheduler(time.time, time.sleep)
-	now = datetime.datetime.now()	
-	log.info('Sending a request to azan API')
-	azan_times = util.api.get_prayer_times(
-		config.default['city'],
-		config.default['state'],
-		config.default['country'],
-		now.date().month,
-		now.date().year)
+    scheduler = sched.scheduler(time.time, time.sleep)
+    now = datetime.datetime.now()
+    log.info('Sending a request to azan API')
+    azan_times = util.api.get_prayer_times(
+        config.default['city'],
+        config.default['state'],
+        config.default['country'],
+        now.date().month,
+        now.date().year)
 
-	for azan_name in AZAN_ENUM:
-		now = datetime.datetime.now()
-		azan_time = util.prayer.format_azan_time(azan_times[azan_name])
-		
-		if now > azan_time:
-			continue
-		
-		log.info('{} is scheduled at {}'.format(azan_name, azan_time))
-		#called = False
-		#while not called:
-		#	now = datetime.datetime.now()
-		#	if 0 <= abs((now - azan_time).total_seconds()) <= 60:
-		#		log.info('Calling {} now'.format(azan_name))
-		#		util.prayer.play()
-		#		called = True
-		#		time.sleep(15)
-		log.info('Schedule {} at {}'.format(azan_name, azan_time))
-		scheduler.enterabs(float(azan_time.strftime('%s')), 1, util.prayer.play, ())
+    water_time = datetime.datetime.now()
+    for azan_name in AZAN_ENUM:
+        now = datetime.datetime.now()
+        azan_time = util.prayer.format_azan_time(azan_times[azan_name])
 
-	scheduler.run()
-	
+        if now > azan_time:
+            continue
+
+        log.info('{} is scheduled at {}'.format(azan_name, azan_time))
+        scheduler.enterabs(float(azan_time.strftime('%s')), 1, util.prayer.play, ())
+
+        water_time += datetime.timedelta(seconds=10)
+
+    scheduler.run()
+
 
 if __name__ == '__main__':
-	try:
-		main()
-	except Exception:
-		log.exception('Oops something went wrong')
-		raise
-		
+    try:
+        main()
+    except Exception:
+        log.exception('Oops something went wrong')
+        raise
--- util/prayer.py
+++ util/prayer.py
@@ -2,18 +2,22 @@ import datetime
 import os
 import sys
 import logger
+import pygame
 
 log = logger.get_logger(__name__)
 
 
 def play(name=None):
     if not name:
-        name = 'azan.mp3'
+        name = 'water.mp3'
     path = os.path.dirname(os.path.abspath(__file__))
     file_path = '{}/../assets/{}'.format(path, name)
     log.info('Calling Azan now')
     log.info(file_path)
-    os.system('cvlc --play-and-exit {}'.format(file_path))
+    # os.system('cvlc --play-and-exit {}'.format(file_path))
+    pygame.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play(0)
 
 
 def format_azan_time(azan_time):
