722c16bed55f8a0722e34713d799c972531852b8
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-05-11 11:40:34 -0700

    Use testing sound

--- util/api.py
+++ util/api.py
@@ -20,17 +20,18 @@ def get_prayer_times(city, state, country, month, year):
         'method': config.default['method']
     }
     response = requests.get(config.default['azan_end_point'], params=payload)
+ 
     if response.status_code != requests.codes.ok:
         raise InvalidRequestError('Oops something went wrong here!')
-
+    log.info('got response from azan api')
     prayers = response.json()['data']
     current_date = (datetime.datetime.now() + datetime.timedelta(days=1)).date()
-    
+
     for prayer in prayers:
         prayer_date = datetime.datetime.strptime(
             prayer['date']['gregorian']['date'], '%d-%m-%Y')
-        
+
         if prayer_date.date() == current_date:
             return prayer['timings']
-            
+
     raise InvalidRequestError('Oops, something went wrong, could not find the correct date')
